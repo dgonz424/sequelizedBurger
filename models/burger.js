418af6b700bdd97f5abbb8a363d09f4cd92ec68c
Daniel Andrew Gonzalez <danielandrewgonzalez@Daniels-MacBook-Pro.local>
2017-05-02 15:08:37 -0400

    bump

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  var Burgers = sequelize.define("burgers", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      validate: {
+        len: [1]
+      }
+    },
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      len: [1]
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      len: [1]
+    }
+  });
+  return Burgers;
+};
